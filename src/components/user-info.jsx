05a6ff413b78bebd90a818bf87c688804078bfae
unknown <stefanhabets92@gmail.com>
2024-03-16 16:37:02 +0100

    generic data source hook

--- src/components/user-info.jsx
+++ src/components/user-info.jsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import { useCurrentUser } from './current-user.hook';
+import { useDataSource } from './data-source.hook';
 import { useResource } from './resource.hook';
 import { useUser } from './user.hook';
 
+const fetchFromServer = (resourceUrl) => async () => {
+  const response = await axios.get(resourceUrl);
+  return response.data;
+};
+
+const getDataFromLocaleStorage = (key) => () => {
+  return localStorage.getItem(key);
+};
+
 export const UserInfo = ({ userId }) => {
-  const user = useResource("/users/2");
+  // const user = useResource("/users/2");
+  const user = useDataSource(fetchFromServer(`/users/${userId}`));
+  const message = useDataSource(getDataFromLocaleStorage("msg"));
+
   const { name, age, country, books } = user || {};
   return user ? (
     <>
